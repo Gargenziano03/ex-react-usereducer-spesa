2162316821f6d74de28be2fbd1959d3ce5a39c69
Gargenziano03 <gargenziano03@gmail.com>
2025-04-05 15:34:21 +0200

    add: Milestone 3

--- CarelloDellaSpesa/src/App.jsx
+++ CarelloDellaSpesa/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import "./App.css"
+
 export default function App() {
   const [addedProducts, setAddedProducts] = useState([])
   const products = [
@@ -13,9 +15,38 @@ export default function App() {
     if (!presenteNelCarrello) {
       const productQantity = { ...product, quantity: 1 }
       setAddedProducts([...addedProducts, productQantity]);
+    } else {
+      updateProductQuantity(product);
     }
-
   }
+
+  const updateProductQuantity = (product) => {
+    const updated = addedProducts.map((p) => {
+      if (p.name === product.name) {
+        return { ...p, quantity: p.quantity + 1 };
+      } else {
+        return p;
+      }
+    });
+    setAddedProducts(updated);
+  }
+
+  const removeToCart = (product) => {
+    const updated = addedProducts
+      .map((p) => {
+        if (p.name === product.name) {
+          if (p.quantity > 1) {
+            return { ...p, quantity: p.quantity - 1 };
+          } else {
+            return null;
+          }
+        }
+        return p;
+      })
+      .filter(Boolean);
+
+    setAddedProducts(updated);
+  };
 
   return (
     <div className="container">
@@ -23,13 +54,14 @@ export default function App() {
       <ul className="product-list">
         {products.map((product, index) => (
           <li key={index} className="product-item">
-            <div className="info">
+            <div>
               <span>{product.name} - €{product.price.toFixed(2)}</span>
             </div>
             <button onClick={() => addToCart(product)}>Aggiungi al carrello</button>
           </li>
         ))}
       </ul>
+
       <h2>Carrello</h2>
       {addedProducts.length === 0 ? (
         <p>Il carrello è vuoto</p>
@@ -38,6 +70,7 @@ export default function App() {
           {addedProducts.map((product, index) => (
             <li key={index} className="cart-item">
               <div> {product.name} - €{product.price.toFixed(2)} - Quantità: {product.quantity}</div>
+              <button onClick={() => removeToCart(product)}>rimuovi dal carrello</button>
             </li>
           ))}
         </ul>
@@ -45,3 +78,5 @@ export default function App() {
     </div>
   );
 }
+
+
